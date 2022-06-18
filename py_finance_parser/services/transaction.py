319b115009d7f2bdda42a355af910758b34e952e
Scott Guertin <sguertin@gmail.com>
2022-06-17 20:52:41 -0400

    Added enumerate to simplify the logic used

--- py_finance_parser/services/transaction.py
+++ py_finance_parser/services/transaction.py
@@ -1,21 +1,24 @@
 import csv
-from models.transaction import Transaction, TransactionList
+from pathlib import Path
+
+from py_finance_parser.models.transaction import Transaction, TransactionList
 
 
-class TransactionParsingService:
-    def __init__(self, file_path: str):
+class TransactionFileParsingService:
+    file_path: Path
+
+    def __init__(self, file_path: Path):
         self.file_path = file_path
 
     def parse_transaction_file(self, starting_id: int = 1) -> TransactionList:
-        result = []
         with open(self.file_path, "r+") as f:
-            contents = csv.reader(f)
-            n = starting_id
-            for row in contents:
-                result.append(Transaction.create_from_csv_row(n, row))
-                n += 1
+            contents = [_ for _ in csv.reader(f)]
+            result = [
+                Transaction.create_from_csv_row(n + starting_id, row)
+                for n, row in enumerate(contents)
+            ]
         return TransactionList(result)
 
-    def save_parsed_transaction_file(self, transactions: TransactionList):
+    def save_parsed_transaction_file(self, transactions: TransactionList) -> None:
         with open(self.file_path, "w+") as f:
             f.write(transactions.to_json())
